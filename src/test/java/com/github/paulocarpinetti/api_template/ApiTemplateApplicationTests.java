6886bed908e66c82708bae17e83f83db68d4b48b
PauloCarpinetti <pauloscarpinetti@gmail.com>
2025-10-22 20:22:39 -0300

    feat: first configuration

---
+++ src/test/java/com/github/paulocarpinetti/api_template/ApiTemplateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.paulocarpinetti.api_template;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiTemplateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
